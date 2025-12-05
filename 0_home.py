9d48d095a632e77ea0d815f0e10a3cb7ac53cd59
Terryl Hassen <terryl.h@free.fr>
2025-12-05 16:48:24 +0100

    Add files via upload

---
+++ 0_home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(page_title="Qualité de l’air – Projet TerryL", layout="wide")
+
+st.title(" Projet TerryL – Dashboard Qualité de l’air")
+
+st.markdown("""
+Bienvenue dans l'application multipage.  
+Utilisez la barre latérale pour naviguer entre les analyses :
+- Visualisation générale  
+- Comparaison des particules  
+- AQI  
+- Détection des pics  
+- Heatmap annuelle  
+""")
